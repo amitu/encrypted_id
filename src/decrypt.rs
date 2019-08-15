ee10123a851703cac54dfad789a5ad107d864d5a
AbrarNitk <abrar.nitk@gmail.com>
2019-08-15 20:27:06 +0530

    add decode ekey util

--- src/decrypt.rs
+++ src/decrypt.rs
@@ -1,4 +1,4 @@
-use crate::{Error, Result};
+use crate::{Error, Result, CONFIG};
 use base64;
 use byteorder::{LittleEndian, ReadBytesExt};
 use crc;
@@ -84,4 +84,13 @@ pub fn decode_ekey(
     }
 
     Ok(id)
+}
+
+
+pub fn decode_ekey_util(ekey: &str, sub_key: &str) -> Result<u64> {
+    let config = CONFIG.read().unwrap();
+    if config.secret_key.is_none() {
+        return Err(Error::SecretKeyNotFound.into())
+    }
+    decode_ekey(ekey, sub_key, config.secret_key.as_ref().unwrap(), config.secret_key_bytes.as_ref())
 }
